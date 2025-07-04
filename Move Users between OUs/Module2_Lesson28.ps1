d692070fe659fecd13a2ad5ca0f1dc83d279bf8e
Qhamaninande Rebe <67279129+QueMaster@users.noreply.github.com>
2025-07-04 10:37:48 +0200

    Add files via upload

---
+++ Move Users between OUs/Module2_Lesson28.ps1
@@ -0,0 +1,16 @@
+﻿#Create Organizational Unit
+
+New-ADOrganizationalUnit -Name 'moved'
+
+
+#Move one user
+
+Move-ADObject -Identity 'CN=test3,OU=test,DC=root,DC=local' -TargetPath 'OU=moved,dc=root,dc=local'
+
+
+#Pipeline move a user
+
+Get-ADUser -Identity 'test3' | Move-ADObject -TargetPath 'OU=moved,dc=root,dc=local'
+
+
+
