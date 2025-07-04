32524dfad74c38af607552f95ea6465bc386e2cc
Qhamaninande Rebe <67279129+QueMaster@users.noreply.github.com>
2025-07-04 10:38:31 +0200

    Add files via upload

---
+++ Filter and Where-Object/Module2_Lesson29.ps1
@@ -0,0 +1,9 @@
+﻿
+
+#retrieve  AD users with property department sales
+
+Get-ADUser -Filter * -Properties * | Where-Object {$PSItem.Department -eq 'Sales' }
+
+#retrieve  AD users with property department sales and move to an OU
+
+Get-ADUser -Filter * -Properties * | Where-Object {$PSItem.Department -eq 'Sales' } | Move-ADObject -TargetPath 'OU=moved,dc=root,dc=local'
