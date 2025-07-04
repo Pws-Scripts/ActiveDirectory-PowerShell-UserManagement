979b909e347a0b0bc6651a20b495b223b6c2823c
Qhamaninande Rebe <67279129+QueMaster@users.noreply.github.com>
2025-07-04 11:08:45 +0200

    Delete Filter and Where-Object directory

--- Filter and Where-Object/Module2_Lesson29.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-
-#retrieve  AD users with property department sales
-
-Get-ADUser -Filter * -Properties * | Where-Object {$PSItem.Department -eq 'Sales' }
-
-#retrieve  AD users with property department sales and move to an OU
-
-Get-ADUser -Filter * -Properties * | Where-Object {$PSItem.Department -eq 'Sales' } | Move-ADObject -TargetPath 'OU=moved,dc=root,dc=local'
